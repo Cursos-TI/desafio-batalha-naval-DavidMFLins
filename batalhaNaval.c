9a4007605df175d210d9488b13269e347d820dc2
DavidMFLins <macauly1990@gmail.com>
2026-03-30 02:42:39 +0000

    Nivel novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,122 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // Tabuleiro consertado: sem o '*' e com o ';' no final
+    int tabuleiro [10][10] = {
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0},
+     {0,0,0,0,0,0,0,0,0,0}
+    };
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int menu;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    do {
+        // O menu veio para DENTRO do 'do' para repetir na tela
+        printf("\n### MENU BATALHA NAVAL ###\n\n");
+        printf("1) posicionar navio na horizontal\n");
+        printf("2) posicionar navio na vertical\n");
+        printf("3) posicionar navio na horizontal\n");
+        printf("4) Resetar tabuleiro\n");
+        printf("5) exibir tabuleiro atual\n");
+        printf("0) Sair\n");
+        printf("Digite uma opção valida\n\n");
+        
+        scanf("%d", &menu); 
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+        switch (menu){
+            case 1:
+            {
+                int selecaoI , selecaoJ , contador;
+                printf("Selecione uma coluna de 0 a 7 pois o navio ocupará o espaço que escolheu mais dois espaços para a direita (tamanho do navio é de 3 casas)\n");
+                scanf("%d" , &selecaoI); 
+                printf("Selecione uma linha de 0 a 9:\n");
+                scanf("%d" , &selecaoJ);
+                contador = selecaoI + 2; 
+                // verificando dados de entrada
+                if ((selecaoI >= 0 && selecaoI <= 7) && (selecaoJ >= 0 && selecaoJ <= 9)){
+                    while (selecaoI <= contador){
+                        tabuleiro [selecaoJ][selecaoI] = 3;
+                        selecaoI++;
+                    }
+                    printf("Navio posicionado!\n");
+                } else {
+                    printf("Selecione uma linha e coluna valida\n");
+                }
+            }
+            break;
+            case 2:
+                        {
+                int selecaoI , selecaoJ , contador;
+                printf("Selecione uma coluna de 0 a 9:\n");
+                scanf("%d" , &selecaoI); 
+                printf("Selecione uma linha de 0 a 7 pois o navio ocupará o espaço que escolheu mais dois espaços para baixo (tamanho do navio é de 3 casas)\n");
+                scanf("%d" , &selecaoJ);
+                contador = selecaoJ + 2; 
+                // verificando dados de entrada
+                if ((selecaoI >= 0 && selecaoI <= 9) && (selecaoJ >= 0 && selecaoJ <= 7)){
+                    while (selecaoJ <= contador){
+                        tabuleiro [selecaoJ][selecaoI] = 3;
+                        selecaoJ++;
+                    }
+                    printf("Navio posicionado!\n");
+                } else {
+                    printf("Selecione uma linha e coluna valida\n");
+                }
+            }
+            break;
+            case 3:
+            {
+                printf("\n--- POSICIONANDO NAVIOS DIAGONAIS ---\n");
+                int linhaDiag1 = 1;
+                int colunaDiag1 = 1;
+                for (int i = 0; i < 3; i++) {
+                    tabuleiro[linhaDiag1 + i][colunaDiag1 + i] = 3;
+                }
+                printf("Navio Diagonal 1 posicionado com sucesso!\n");
+                int linhaDiag2 = 5;
+                int colunaDiag2 = 8;
+                for (int i = 0; i < 3; i++) {
+                    tabuleiro[linhaDiag2 + i][colunaDiag2 - i] = 3;
+                }
+                printf("Navio Diagonal 2 posicionado com sucesso!\n");
+            }
+            break;
+            case 4:
+            for (int l = 0; l < 10; l++) {
+                    for (int c = 0; c < 10; c++) {
+                        tabuleiro [l][c] = 0;
+                    }
+                }
+            printf("Tabuleiro resetado\n");
+            break;
+            case 5:
+            //imprimindo tabuleiro
+            printf("\n--- TABULEIRO ATUAL ---\n");
+            printf("  ");
+                for (int c = 0; c < 10; c++) {
+                    printf("%d ", c);
+                }
+                printf("\n");
+                for (int l = 0; l < 10; l++) {
+                    printf("%d ", l);
+                    for (int c = 0; c < 10; c++) {
+                        printf("%d ", tabuleiro[l][c]);
+                    }
+                    printf("\n");
+                }
+                break;
+            default:
+            break;
+            }
+    } while (menu != 0); //sai quando for 0
 
     return 0;
 }
