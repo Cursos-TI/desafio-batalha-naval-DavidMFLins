207ee8f9cf943c01e9cfa2578e060c94a5fbc586
DavidMFLins <macauly1990@gmail.com>
2026-03-30 21:18:17 +0000

    desafio aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
+#define LINHAS 10
+#define COLUNAS 10
+#define TAMANHO_NAVIO 3
 
 int main() {
 
-    // Tabuleiro consertado: sem o '*' e com o ';' no final
-    int tabuleiro [10][10] = {
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0},
-     {0,0,0,0,0,0,0,0,0,0}
-    };
-
+    int tabuleiro [LINHAS][COLUNAS] = {0};
     int menu;
 
     do {
-        // O menu veio para DENTRO do 'do' para repetir na tela
         printf("\n### MENU BATALHA NAVAL ###\n\n");
         printf("1) posicionar navio na horizontal\n");
         printf("2) posicionar navio na vertical\n");
@@ -35,13 +24,13 @@ int main() {
             case 1:
             {
                 int selecaoI , selecaoJ , contador;
-                printf("Selecione uma coluna de 0 a 7 pois o navio ocupará o espaço que escolheu mais dois espaços para a direita (tamanho do navio é de 3 casas)\n");
+                printf("Selecione uma coluna de 0 a %d pois o navio ocupara o espaco que escolheu mais dois espacos para a direita (tamanho do navio eh de 3 casas)\n", COLUNAS - TAMANHO_NAVIO);
                 scanf("%d" , &selecaoI); 
-                printf("Selecione uma linha de 0 a 9:\n");
+                printf("Selecione uma linha de 0 a %d:\n", LINHAS - 1);
                 scanf("%d" , &selecaoJ);
-                contador = selecaoI + 2; 
+                contador = selecaoI + (TAMANHO_NAVIO - 1); 
                 // verificando dados de entrada
-                if ((selecaoI >= 0 && selecaoI <= 7) && (selecaoJ >= 0 && selecaoJ <= 9)){
+                if ((selecaoI >= 0 && selecaoI <= (COLUNAS - TAMANHO_NAVIO)) && (selecaoJ >= 0 && selecaoJ <= (LINHAS - 1))){
                     while (selecaoI <= contador){
                         tabuleiro [selecaoJ][selecaoI] = 3;
                         selecaoI++;
@@ -55,13 +44,13 @@ int main() {
             case 2:
                         {
                 int selecaoI , selecaoJ , contador;
-                printf("Selecione uma coluna de 0 a 9:\n");
+                printf("Selecione uma coluna de 0 a %d:\n", COLUNAS - 1);
                 scanf("%d" , &selecaoI); 
-                printf("Selecione uma linha de 0 a 7 pois o navio ocupará o espaço que escolheu mais dois espaços para baixo (tamanho do navio é de 3 casas)\n");
+                printf("Selecione uma linha de 0 a %d pois o navio ocupara o espaco que escolheu mais dois espacos para baixo (tamanho do navio eh de 3 casas)\n", LINHAS - TAMANHO_NAVIO);
                 scanf("%d" , &selecaoJ);
-                contador = selecaoJ + 2; 
+                contador = selecaoJ + (TAMANHO_NAVIO - 1);  
                 // verificando dados de entrada
-                if ((selecaoI >= 0 && selecaoI <= 9) && (selecaoJ >= 0 && selecaoJ <= 7)){
+                if ((selecaoI >= 0 && selecaoI <= (COLUNAS - 1)) && (selecaoJ >= 0 && selecaoJ <= (LINHAS - TAMANHO_NAVIO))){
                     while (selecaoJ <= contador){
                         tabuleiro [selecaoJ][selecaoI] = 3;
                         selecaoJ++;
@@ -75,23 +64,45 @@ int main() {
             case 3:
             {
                 printf("\n--- POSICIONANDO NAVIOS DIAGONAIS ---\n");
+                // navio 1
                 int linhaDiag1 = 1;
                 int colunaDiag1 = 1;
-                for (int i = 0; i < 3; i++) {
-                    tabuleiro[linhaDiag1 + i][colunaDiag1 + i] = 3;
+                int sobreposicao1 = 0; 
+                for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                    if (tabuleiro[linhaDiag1 + i][colunaDiag1 + i] == 3) {
+                        sobreposicao1 = 1;
+                    }
                 }
-                printf("Navio Diagonal 1 posicionado com sucesso!\n");
+                if (sobreposicao1 == 0) {
+                    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                        tabuleiro[linhaDiag1 + i][colunaDiag1 + i] = 3;
+                    }
+                    printf("Navio Diagonal 1 posicionado com sucesso!\n");
+                } else {
+                    printf("ERRO: Ja existe um navio no caminho do Diagonal 1!\n");
+                }
+                //diagonal 2
                 int linhaDiag2 = 5;
                 int colunaDiag2 = 8;
-                for (int i = 0; i < 3; i++) {
-                    tabuleiro[linhaDiag2 + i][colunaDiag2 - i] = 3;
+                int sobreposicao2 = 0;
+                for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                    if (tabuleiro[linhaDiag2 + i][colunaDiag2 - i] == 3) {
+                        sobreposicao2 = 1; 
+                    }
                 }
-                printf("Navio Diagonal 2 posicionado com sucesso!\n");
+                if (sobreposicao2 == 0) {
+                    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                        tabuleiro[linhaDiag2 + i][colunaDiag2 - i] = 3;
+                    }
+                    printf("Navio Diagonal 2 posicionado com sucesso!\n");
+                } else {
+                    printf("ERRO: Ja existe um navio no caminho do Diagonal 2!\n");
+                }
             }
             break;
             case 4:
-            for (int l = 0; l < 10; l++) {
-                    for (int c = 0; c < 10; c++) {
+            for (int l = 0; l < LINHAS; l++) {
+                    for (int c = 0; c < COLUNAS; c++) {
                         tabuleiro [l][c] = 0;
                     }
                 }
@@ -101,13 +112,13 @@ int main() {
             //imprimindo tabuleiro
             printf("\n--- TABULEIRO ATUAL ---\n");
             printf("  ");
-                for (int c = 0; c < 10; c++) {
+                for (int c = 0; c < COLUNAS; c++) {
                     printf("%d ", c);
                 }
                 printf("\n");
-                for (int l = 0; l < 10; l++) {
+                for (int l = 0; l < LINHAS; l++) {
                     printf("%d ", l);
-                    for (int c = 0; c < 10; c++) {
+                    for (int c = 0; c < COLUNAS; c++) {
                         printf("%d ", tabuleiro[l][c]);
                     }
                     printf("\n");
